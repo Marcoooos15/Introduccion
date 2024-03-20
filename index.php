61e5830851d17d5bce9941e772dabcb81736cd82
Marcos Martinez <marcosdaniel.martinez.914@gmail.com>
2024-03-20 09:03:48 -0300

    Prueba <<<END

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <?php echo "Hola Mundo!"; echo "<h2>Adios Mundo</h2>" ?>
     <?php echo <<<END
             En un mundo conectado, donde cada segundo se envían
-            millones de correos electrónicos....
+            millones de correos electrónicos.... Nacional sigue siendo el más grande de Uruguay
             END; ?>
 </body>
 
