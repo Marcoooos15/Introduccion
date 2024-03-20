59a51322c30116549c569314f37e9031d8795444
Marcos Martinez <marcosdaniel.martinez.914@gmail.com>
2024-03-20 08:40:31 -0300

    <h3> tag added

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 </head>
 
 <body>
-    <?php echo "Hola Mundo!"; echo "<h2>Adios Mundo<h2>" ?>
-    <?php echo "Prueba1" ?>
+    <?php echo "Hola Mundo!"; echo "<h2>Adios Mundo</h2>" ?>
+    <?php echo "<h3> Prueba1</h3>" ?>
 </body>
 
 </html>
