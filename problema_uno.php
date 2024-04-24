8681714c6c6ddea91aa9886084bb0f5726957237
Marcos Martinez <marcosdaniel.martinez.914@gmail.com>
2024-04-23 23:37:29 -0300

    tarea verdulería pepe, php y estilos

--- problema_uno.php
+++ problema_uno.php
@@ -2,25 +2,43 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=<, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
     <title>Verdulería Pepe</title>
 </head>
 <body>
+    <div class="container flex-container pricing-form">
     <h1>Consumo Final - Verdulería Pepe</h1>
     <p>A continuación se detallan los totales de su compra.<p>
     <?php
-        $cantManzanas = 5;
-        $cantBananas = 2;
+        $getCantidadManzanas = 0;
+        $getCantidadBananas = 0;
         $kgManzana = 200;
         $kgBanana = 500;
-        $precioManz = $cantManzanas * $kgManzana;
-        $precioBana = $cantBananas * $kgBanana;
-        $totalPrecio = $precioManz + $precioBana;
 
-        echo "El precio de la manzana (por kg) es de $$kgManzana <br>";
-        echo "El precio de la banana (por kg) es de $$kgBanana <br>";
-        echo "Total de Compra: $$totalPrecio";
-    ?>
-    
+        if (isset($_POST['submit'])) {
+            $getCantidadBananas = (int)$_POST['banana'];
+            $getCantidadManzana = (int)$_POST['manzana'];
+        }
+        
+        $precioManz = $getCantidadManzanas * $kgManzana;
+        $precioBana = $getCantidadBananas * $kgBanana;
+        $totalPrecio = $precioManz + $precioBana;
+        ?>
+
+        <form action="" method="post">
+            <input type="number" name="banana" placeholder="Bananas" min="0">
+            <input type="number" name="manzana" placeholder="Manzanas" min="0">
+            <input type="submit" value="Aceptar" name="submit" id="submitBtn">
+        </form>
+
+        <div>
+            <p><?php echo "El precio de la manzana (por kg) es de $$kgManzana <br>";?></p>
+            <p><?php echo "El precio de la banana (por kg) es de $$kgBanana <br>";?></p>
+        </div>
+
+        <div class="container flex-container">
+            <p><?php echo "Total de Compra: $$totalPrecio";?></p>
+        </div>
 </body>
 </html>
