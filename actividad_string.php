c297bf4d7b46a39523d02b1bc4b9cf1d9d59be7e
Marcos Martinez <marcosdaniel.martinez.914@gmail.com>
2024-08-14 08:34:07 -0300

    Actualización pequeña actividad print_r y microrelatos

--- actividad_string.php
+++ actividad_string.php
@@ -24,7 +24,7 @@
         $lista = "manzana,naranja,banana,frutilla";
         $frutas = explode("," , $lista);
         print_r($frutas);
-    ?> -->
+    ?> Fin del comentario -->
 
     <?php
     $microrelato = 'El Club Nacional de Football es una institución deportiva uruguaya. Fue fundado el 14 de mayo de 1899 en Montevideo, por iniciativa de jóvenes estudiantes de la época, con la intención de consolidar una institución de fútbol para uruguayos criollos frente al predominio de clubes y practicantes extranjeros europeos de este deporte, particularmente ingleses y alemanes, siendo considerado así el «primer equipo criollo» del país y uno de los primeros clubes fundados por nacionales en América.';
