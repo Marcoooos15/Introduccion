7a8ba585f1c7b84fd8a70ed8b4d1704c114c43b6
Marcos Martinez <marcosdaniel.martinez.914@gmail.com>
2024-08-14 08:35:05 -0300

    Variables superglobales de php y sesiones.

---
+++ sesiones.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    session_start();
+    $usuario = [
+        "correo" => "user1@gmail.com",
+        "pais" => "Uruguay",
+        "contraseña" => "1234"
+    ];
+    $_SESSION['user'] = $usuario;
+    print_r($_SESSION);
+
+    ?>
+</body>
+</html>
